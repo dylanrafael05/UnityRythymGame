8fb4f700fa70cbbe0041a54a55ad07ae3f865752
Dylan Rafael <dylanrafael05@gmail.com>
2020-01-25 22:30:24 -0500

    Added somne code for timings structure

---
+++ Unity Rythym Game/Assets/Scripts/Global (Static)/SyncHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncHandler
+{
+
+    //SONG DATA//
+    private static double startTime;
+    private static BPMStructure bpmStructure;
+
+    void startSong(BPMStructure BPMStructure, AudioClip song) { 
+
+        startTime = Time.time;
+        bpmStructure = BPMStructure;
+
+    }
+
+}
