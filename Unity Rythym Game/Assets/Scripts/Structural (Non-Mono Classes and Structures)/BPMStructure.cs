4661ce1b1e11a1859367eac7fbe75fe4143fe5dd
Dylan Rafael <dylanrafael05@gmail.com>
2020-01-25 22:30:24 -0500

    Added some code for timing structure

---
+++ Unity Rythym Game/Assets/Scripts/Structural (Non-Mono Classes and Structures)/BPMStructure.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BPMStructure
+{
+
+    public Dictionary<double, double> keyBPMPoints;
+
+    public BPMStructure(Dictionary<double, double> keyBPMPoints) { this.keyBPMPoints = keyBPMPoints; }
+
+}
